07e40739d275168c39f544ce8dc18c11e7897ebe
Adam Giacomelli <adam.giacom@gmail.com>
2018-07-17 12:16:02 +0200

    Updated to allow module specific tests

---
+++ app/src/modules/example/tests/e2e/exampleTest.js
@@ -0,0 +1,29 @@
+const test = require("ava");
+const request = require("supertest");
+const app = require("../../../../../server.js");
+
+const PREFIX = "/api/v1";
+
+test.serial("get /example/", async t => {
+  t.plan(1);
+
+  const res = await request(app).get(`${PREFIX}/example`);
+
+  t.is(res.status, 200);
+});
+
+test.serial("get /example/error", async t => {
+  t.plan(1);
+
+  const res = await request(app).get(`${PREFIX}/example/error`);
+
+  t.is(res.status, 500);
+});
+
+test.serial("get /example/missing", async t => {
+  t.plan(1);
+
+  const res = await request(app).get(`${PREFIX}/example/missing`);
+
+  t.is(res.status, 404);
+});
